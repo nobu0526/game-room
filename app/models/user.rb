967148ba0e331d262dea52846a191a250d5a1a98
nobu0526 <nobu19930526@gmail.com>
2021-06-04 15:20:05 +0900

    修正なし

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true、
+         validates :nickname, presence: true
 end
