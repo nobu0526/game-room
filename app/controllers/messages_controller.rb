205e8276fb5066818e8b40fb5a6df456a4e0093f
nobu0526 <nobu19930526@gmail.com>
2021-07-12 14:55:53 +0900

    修正２

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
+    @user = User.find(params[:room_id])
    end
  
    def create
